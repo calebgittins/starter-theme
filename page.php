acf8a7ea4ee4b88ffbc9b76fc6de60052c618391
Caleb Gittins <caleb@thirteendigital.com.au>
2020-08-05 17:01:11 +1000

    Update page.php

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 	<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
-		<?php if ( get_field('td_page_content') ) : ?>
+		<?php if ( get_field('td_page_content') && !post_password_required() ) : ?>
         	<?php get_template_part('inc/flexible'); ?>
         <?php else: ?>
         	<div class="wrap wysiwyg">
