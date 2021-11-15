eccf7ce6539f6dbee13777e57cc4f91d8e3c1d8f
Caleb Gittins <caleb@thirteendigital.com.au>
2021-11-15 10:44:56 +1000

    Update functions.php
    Add WooCommerce conditional statement

--- functions.php
+++ functions.php
@@ -127,4 +127,15 @@
 	function remove_update_themes( $value ) {
 	    return null;
 	}
+
+// Check if WooCommerce is activated
+	if ( ! function_exists( 'is_woocommerce_activated' ) ) {
+		function is_woocommerce_activated() {
+			if ( class_exists( 'woocommerce' ) ) { 
+				return true; 
+			} else { 
+				return false; 
+			}
+		}
+	}
 ?>
